9cc64163bf0470cf7e68dd00651bc1c9a3481f27
瑜笙 <1365841924@qq.com>
2023-01-31 00:10:55 +0800

    fun初始化项目

---
+++ src/main/java/org/supermq/Main.java
@@ -0,0 +1,11 @@
+package org.supermq;
+
+/**
+ * @Author 瑜笙
+ * @Date 2023/1/31 0:07
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
